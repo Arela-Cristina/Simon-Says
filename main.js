297c6c2f11a6ff1a997173e7ebd591f0c1387c9c
Arela-Cristina <instagrandine@gmail.com>
2024-10-24 16:10:50 +0200

    set time interval for boxNum

--- main.js
+++ main.js
@@ -2,3 +2,13 @@
 // Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce.
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 // NOTA: non è importante l'ordine con cui l'utente inserisce i numeri, basta che ne indovini il più possibile.
+
+//contenitore num
+const numBox = document.getElementById('numBox');
+
+// Dopo 30 secondi i numeri scompaiono
+setTimeout(function() {
+    console.log("prova");
+    // prova
+    numBox.style.display = 'none';
+}, 3000);
